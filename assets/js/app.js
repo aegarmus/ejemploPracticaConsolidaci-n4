3f685d218dabd99ea405b2e4c99e8c05aa47cf38
aegarmus <aegarmus@gmail.com>
2023-06-16 15:21:37 -0400

    Creado función fetch API

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+//3.1 Definimos constante global con la url base a consumir
+const URL_BASE = "https://swapi.dev/api/"
 
 //1- Definir Clase consrtructura
 class Personaje {
@@ -15,3 +17,18 @@ const crearPersonaje = (data) => {
 }
 
 //3. Consumo API
+
+const traerPersonaje = async(id) => {
+    try{
+        let resultado = await fetch(`${URL_BASE}people/${id}`)
+        let respuesta = await resultado.json();
+        let personaje = crearPersonaje(respuesta)
+        console.log(personaje)
+    }catch (err){
+        throw new Error (err)
+    }
+
+} 
+
+
+
